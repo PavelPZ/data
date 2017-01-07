b8f578f62164a79460969ddc673b9f780e72f5f3
PZ <pzika@langmaster.cz>
2017-01-07 15:13:32 +0100

    v

--- rw-course/index.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-export * from './objects';
-export * from './course';
-export * from './lib';
-
-//export default course;
-
-//export = course;
-//export as namespace course;
